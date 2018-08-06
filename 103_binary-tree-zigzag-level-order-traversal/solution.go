7cb4ead2dac16f0fa86eecd3d860407dbcb0cb69
chyeh <chyeh.taiwan@gmail.com>
2018-08-06 13:44:10 +0300

    103 - Binary Tree Zigzag Level Order Traversal

---
+++ 103_binary-tree-zigzag-level-order-traversal/solution.go
@@ -0,0 +1,76 @@
+package solution
+
+import "container/list"
+
+type queue struct {
+	*list.List
+}
+
+func NewQueue() *queue {
+	return &queue{
+		list.New(),
+	}
+}
+
+func (q *queue) push(d interface{}) {
+	q.PushBack(d)
+}
+
+func (q *queue) pop() interface{} {
+	return q.Remove(q.Front())
+}
+
+func (q *queue) isEmpty() bool {
+	return q.Len() == 0
+}
+
+type TreeNode struct {
+	Left  *TreeNode
+	Val   int
+	Right *TreeNode
+}
+
+type TreeNodeWithLevel struct {
+	node  *TreeNode
+	level int
+}
+
+func reverse(slc []int) []int {
+	var ans []int
+	for i := len(slc) - 1; i >= 0; i-- {
+		ans = append(ans, slc[i])
+	}
+	return ans
+}
+
+func zigzagLevelOrder(root *TreeNode) [][]int {
+	ans := [][]int{}
+	if root == nil {
+		return ans
+	}
+	q := NewQueue()
+	q.push(TreeNodeWithLevel{root, 0})
+
+	for !q.isEmpty() {
+		popped := q.pop().(TreeNodeWithLevel)
+		if popped.level == len(ans) {
+			ans = append(ans, []int{})
+		}
+		ans[popped.level] = append(ans[popped.level], popped.node.Val)
+
+		if popped.node.Left != nil {
+			q.push(TreeNodeWithLevel{popped.node.Left, popped.level + 1})
+		}
+		if popped.node.Right != nil {
+			q.push(TreeNodeWithLevel{popped.node.Right, popped.level + 1})
+		}
+	}
+
+	for i, row := range ans {
+		if i%2 == 1 {
+			ans[i] = reverse(row)
+		}
+	}
+
+	return ans
+}
