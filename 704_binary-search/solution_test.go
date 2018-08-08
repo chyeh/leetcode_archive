61cd7f342d20defd929fbf743a44fd64560ab9b8
chyeh <chyeh.taiwan@gmail.com>
2018-08-08 18:51:40 +0300

    704 - Binary Search

---
+++ 704_binary-search/solution_test.go
@@ -0,0 +1,25 @@
+package solution
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSearch(t *testing.T) {
+	tests := []struct {
+		inputNums   []int
+		inputTarget int
+		expected    int
+	}{
+		{[]int{-1, 0, 3, 5, 9, 12}, 9, 4},
+		{[]int{-1, 0, 3, 5, 9, 12}, 2, -1},
+	}
+
+	for i, v := range tests {
+		actual := search(v.inputNums, v.inputTarget)
+		if !reflect.DeepEqual(actual, v.expected) {
+			t.Errorf("Error on case %d: %v(actual) != %v(expected)", i, actual, v.expected)
+		}
+		t.Logf("Case %d: %v(actual) == %v(expected)", i, actual, v.expected)
+	}
+}
