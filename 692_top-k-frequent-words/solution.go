6774f6a973b065355ea92cf6866aec435db6991c
chyeh <chyeh.taiwan@gmail.com>
2018-09-17 15:58:15 +0300

    [UPDATE] 692 - Top K Frequent Words

--- 692_top-k-frequent-words/solution.go
+++ 692_top-k-frequent-words/solution.go
@@ -1,59 +1,80 @@
 package solution
 
-func heapify(freq map[string]int, heap []string, heapIndex map[string]int, index int) {
-	largestIndex := index
-	l := (2 * index) + 1
-	r := (2 * index) + 2
-	if l < len(heap) &&
-		((freq[heap[l]] > freq[heap[largestIndex]]) || (freq[heap[l]] == freq[heap[largestIndex]] && heap[l] < heap[largestIndex])) {
-		largestIndex = l
-	}
-	if r < len(heap) &&
-		((freq[heap[r]] > freq[heap[largestIndex]]) || (freq[heap[r]] == freq[heap[largestIndex]] && heap[r] < heap[largestIndex])) {
-		largestIndex = r
-	}
-	if largestIndex != index {
-		heap[largestIndex], heap[index] = heap[index], heap[largestIndex]
-		heapIndex[heap[largestIndex]], heapIndex[heap[index]] = largestIndex, index
-		heapify(freq, heap, heapIndex, largestIndex)
-	}
+type PriorityQueue struct {
+	heap []string
+	freq map[string]int
 }
 
-func heapIncreasKey(freq map[string]int, heap []string, heapIndex map[string]int, word string) {
-	freq[word]++
-	index := heapIndex[word]
+func NewPriorityQueue(freq map[string]int) *PriorityQueue {
+	pq := &PriorityQueue{
+		freq: freq,
+	}
+	for word := range freq {
+		pq.Insert(word)
+	}
+	return pq
+}
+
+func (q *PriorityQueue) Insert(word string) {
+	q.heap = append(q.heap, word)
+	index := len(q.heap) - 1
 	parent := (index - 1) / 2
-	for freq[heap[parent]] < freq[heap[index]] || (freq[heap[parent]] == freq[heap[index]] && heap[parent] > heap[index]) {
-		heap[parent], heap[index] = heap[index], heap[parent]
-		heapIndex[heap[parent]], heapIndex[heap[index]] = parent, index
+	for index > 0 && q.greaterThan(q.heap[index], q.heap[parent]) {
+		q.heap[index], q.heap[parent] = q.heap[parent], q.heap[index]
 		index = (index - 1) / 2
 		parent = (parent - 1) / 2
 	}
 }
 
-func extractMax(freq map[string]int, heap *[]string, heapIndex map[string]int) string {
-	max := (*heap)[0]
-	(*heap)[0], (*heap)[len((*heap))-1] = (*heap)[len((*heap))-1], (*heap)[0]
-	heapIndex[(*heap)[0]], heapIndex[(*heap)[len((*heap))-1]] = 0, len((*heap))-1
-	(*heap) = (*heap)[:len((*heap))-1]
-	heapify(freq, (*heap), heapIndex, 0)
+func (q PriorityQueue) greaterThan(a, b string) bool {
+	if q.freq[a] > q.freq[b] {
+		return true
+	} else if q.freq[a] == q.freq[b] {
+		if a < b {
+			return true
+		}
+	}
+	return false
+}
+
+func (q *PriorityQueue) ExtractMax() string {
+	max := q.heap[0]
+	q.heap[0], q.heap[len(q.heap)-1] = q.heap[len(q.heap)-1], q.heap[0]
+	q.heap = q.heap[:len(q.heap)-1]
+	q.heapify(0)
 	return max
+}
+
+func (q *PriorityQueue) heapify(index int) {
+	largest := index
+	l := (2 * index) + 1
+	r := (2 * index) + 2
+	if l < len(q.heap) && q.greaterThan(q.heap[l], q.heap[largest]) {
+		largest = l
+	}
+	if r < len(q.heap) && q.greaterThan(q.heap[r], q.heap[largest]) {
+		largest = r
+	}
+	if largest != index {
+		q.heap[index], q.heap[largest] = q.heap[largest], q.heap[index]
+		q.heapify(largest)
+	}
 }
 
 func topKFrequent(words []string, k int) []string {
 	freq := make(map[string]int, len(words))
-	var heap []string
-	heapIndex := make(map[string]int, len(words))
 	for _, w := range words {
 		if _, ok := freq[w]; !ok {
-			heap = append(heap, w)
-			heapIndex[w] = len(heap) - 1
+			freq[w] = 1
+		} else {
+			freq[w]++
 		}
-		heapIncreasKey(freq, heap, heapIndex, w)
 	}
+	pq := NewPriorityQueue(freq)
+
 	var ans []string
 	for i := 0; i < k; i++ {
-		ans = append(ans, extractMax(freq, &heap, heapIndex))
+		ans = append(ans, pq.ExtractMax())
 	}
 	return ans
 }
