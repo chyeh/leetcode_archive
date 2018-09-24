6348ef84db910e17556c126093241864c0fc2e9c
chyeh <chyeh.taiwan@gmail.com>
2018-09-24 22:39:52 +0300

    [Update] 141 - Linked List Cycle

--- 141_linked-list-cycle_GOLANG_UNSUPPORTED/solution.go
+++ 141_linked-list-cycle_GOLANG_UNSUPPORTED/solution.go
@@ -9,13 +9,13 @@ func hasCycle(head *ListNode) bool {
 	if head == nil || head.Next == nil {
 		return false
 	}
-	slow, fast := head, head.Next.Next
+	slow, fast := head, head
 	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
 		if slow == fast {
 			return true
 		}
-		slow = slow.Next
-		fast = fast.Next.Next
 	}
 	return false
 }
@@ -23,17 +23,17 @@ func hasCycle(head *ListNode) bool {
 /* Interpreted to C++ for varification
 class Solution {
 public:
-	bool hasCycle(ListNode *head) {
-		if (!head || !head->next) {
-			return false;
-		}
-		ListNode *slow = head, *fast = head->next->next;
-		while (fast && fast->next) {
-			if (slow == fast) return true;
-			slow = slow->next;
-			fast = fast->next->next;
-		}
-		return false;
-	}
+    bool hasCycle(ListNode *head) {
+        if (!head || !head->next) {
+            return false;
+        }
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
 };
 */
