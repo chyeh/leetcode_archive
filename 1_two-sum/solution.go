bd3110ff6518cac946d5dfcd469f7a029b7e5afc
chyeh <chyeh.taiwan@gmail.com>
2018-09-27 15:13:57 +0300

    [Update] 1 - Two Sum

--- 1_two-sum/solution.go
+++ 1_two-sum/solution.go
@@ -1,12 +1,12 @@
 package solution
 
 func twoSum(nums []int, target int) []int {
-	for i := 0; i < len(nums)-1; i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				return []int{i, j}
-			}
+	valueToIndex := make(map[int]int)
+	for i, num := range nums {
+		if index, ok := valueToIndex[target-num]; ok {
+			return []int{index, i}
 		}
+		valueToIndex[num] = i
 	}
 	return nil
 }
