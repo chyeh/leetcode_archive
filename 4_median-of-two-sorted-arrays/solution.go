2eef376fdd01f9cc9608d72540a257ad841bb3b2
chyeh <chyeh.taiwan@gmail.com>
2018-11-03 17:40:06 +0300

    [Update] 4 - Median of Two Sorted Arrays

--- 4_median-of-two-sorted-arrays/solution.go
+++ 4_median-of-two-sorted-arrays/solution.go
@@ -1,5 +1,7 @@
 package solution
 
+// Solution I: Merge. Time complexity O(M+N); Space complexity O(M+N).
+/*
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	l := (len(nums1) + len(nums2))
 	merged := merge(nums1, nums2)
@@ -43,3 +45,39 @@ func merge(nums1 []int, nums2 []int) []int {
 	}
 	return res
 }
+*/
+
+// Solution II: Two Pointers. Time complexity O(M+N); Space complexity O(1).
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	l := (len(nums1) + len(nums2))
+	var ans, prev float64
+	cnt := 0
+	i, j := 0, 0
+	for ; cnt <= l/2; cnt++ {
+		prev = ans
+		if i == len(nums1) {
+			ans = float64(nums2[j])
+			j++
+			continue
+		}
+		if j == len(nums2) {
+			ans = float64(nums1[i])
+			i++
+			continue
+		}
+		if nums1[i] < nums2[j] {
+			ans = float64(nums1[i])
+			i++
+		} else {
+			ans = float64(nums2[j])
+			j++
+		}
+	}
+	if l%2 != 0 {
+		return ans
+	}
+	return (ans + prev) / 2
+}
+
+// Solution III: Binary serach. Time complexity O(lg(M+N)); Space complexity O(1).
+// Pending...
--- 4_median-of-two-sorted-arrays/solution_test.go
+++ 4_median-of-two-sorted-arrays/solution_test.go
@@ -5,38 +5,6 @@ import (
 	"testing"
 )
 
-func TestMerge(t *testing.T) {
-	tests := []struct {
-		inputA1  []int
-		inputA2  []int
-		expected []int
-	}{
-		{[]int{1, 16, 28, 33, 81, 83}, []int{5, 62, 78, 85}, []int{1, 5, 16, 28, 33, 62, 78, 81, 83, 85}},
-		{[]int{}, []int{}, []int{}},
-		{nil, nil, nil},
-		{nil, []int{}, nil},
-		{nil, []int{1}, nil},
-		{[]int{}, nil, nil},
-		{[]int{1}, nil, nil},
-		{[]int{1}, []int{1}, []int{1, 1}},
-		{[]int{1}, []int{2}, []int{1, 2}},
-		{[]int{2}, []int{1}, []int{1, 2}},
-		{[]int{1, 2}, []int{}, []int{1, 2}},
-		{[]int{}, []int{1, 2}, []int{1, 2}},
-		{[]int{1, 3}, []int{2}, []int{1, 2, 3}},
-		{[]int{1, 2}, []int{3, 4}, []int{1, 2, 3, 4}},
-		{[]int{1, 1, 2}, []int{2, 2}, []int{1, 1, 2, 2, 2}},
-		{[]int{1, 1, 2}, []int{1, 2}, []int{1, 1, 1, 2, 2}},
-	}
-	for i, v := range tests {
-		actual := merge(v.inputA1, v.inputA2)
-		if !reflect.DeepEqual(actual, v.expected) {
-			t.Errorf("Error on case %d: %v(actual) != %v(expected)", i, actual, v.expected)
-		}
-		t.Logf("Case %d: %v(actual) == %v(expected)", i, actual, v.expected)
-	}
-}
-
 func TestFindMedianSortedArrays(t *testing.T) {
 	tests := []struct {
 		inputA1  []int
@@ -45,6 +13,7 @@ func TestFindMedianSortedArrays(t *testing.T) {
 	}{
 		{[]int{1, 3}, []int{2}, 2.0},
 		{[]int{1, 2}, []int{3, 4}, 2.5},
+		{[]int{}, []int{3, 4}, 3.5},
 	}
 	for i, v := range tests {
 		actual := findMedianSortedArrays(v.inputA1, v.inputA2)
