8fe3c16598675b2c73e79f179ca36c6db86aa039
chyeh <chyeh.taiwan@gmail.com>
2018-11-03 17:06:56 +0300

    [Update] 704 - Binary Search

--- 704_binary-search/solution.go
+++ 704_binary-search/solution.go
@@ -1,5 +1,6 @@
 package solution
 
+// Solution I: Iteration
 func search(nums []int, target int) int {
 	l, r := 0, len(nums)-1
 	for l <= r {
@@ -15,3 +16,24 @@ func search(nums []int, target int) int {
 	}
 	return -1
 }
+
+// Solution II: Recursion
+/*
+func find(nums []int, l, r, target int) int {
+    if l > r {
+        return -1
+    }
+    m := l + (r-l)/2
+    if target == nums[m] {
+        return m
+    }
+    if target < nums[m] {
+        return find(nums, l, m-1, target)
+    }
+    return find(nums, m+1, r, target)
+}
+
+func search(nums []int, target int) int {
+    return find(nums, 0, len(nums)-1, target)
+}
+*/
