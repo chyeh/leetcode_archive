f82b2571680ab7290db42e514faa704d095d1ca8
chyeh <chyeh.taiwan@gmail.com>
2018-11-03 10:54:26 +0300

    [Update] 338 - Counting Bits

--- 338_counting-bits/solution.go
+++ 338_counting-bits/solution.go
@@ -1,21 +1,34 @@
 package solution
 
-var res []int
-
-func recur(n int) int {
-	if n == 0 {
-		return 0
-	}
-	if res[n] != 0 {
-		return res[n]
-	}
-	return n%2 + recur(n/2)
-}
-
+// Solution I: Most Significant Bit
 func countBits(num int) []int {
-	res = make([]int, num+1)
-	for i := 0; i <= num; i++ {
-		res[i] = recur(i)
+	dp := make([]int, num+1)
+	for b := 1; b <= num; b = b * 2 {
+		for i := 0; i < b && b+i <= num; i++ {
+			dp[b+i] = dp[i] + 1
+		}
 	}
-	return res
+	return dp
 }
+
+// Solution II: Least Significant Bit
+/*
+func countBits(num int) []int {
+	dp := make([]int, num+1)
+	for i := 1; i <= num; i++ {
+		dp[i] = dp[i>>2] + (i & 1)
+	}
+	return dp
+}
+*/
+
+// Solution III: Last Set Bit
+/*
+func countBits(num int) []int {
+	dp := make([]int, num+1)
+	for i := 1; i <= num; i++ {
+		dp[i] = dp[i&(i-1)] + 1
+	}
+	return dp
+}
+*/
