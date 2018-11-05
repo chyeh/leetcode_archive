0faf767bcb52b46e7654e3423bb8a4cb58551af7
chyeh <chyeh.taiwan@gmail.com>
2018-11-05 18:47:51 +0300

    [Update] 32 - Longest Valid Parentheses

--- 32_longest-valid-parentheses/solution.go
+++ 32_longest-valid-parentheses/solution.go
@@ -1,5 +1,7 @@
 package solution
 
+// Solution I: Dynamic Programming
+/*
 func longestValidParentheses(s string) int {
 	if len(s) < 2 {
 		return 0
@@ -28,6 +30,57 @@ func longestValidParentheses(s string) int {
 
 		if dp[i] > max {
 			max = dp[i]
+		}
+	}
+	return max
+}
+*/
+
+// Solution II: Stack
+type stack struct {
+	data []int
+}
+
+func newStack() *stack {
+	return &stack{
+		data: make([]int, 0),
+	}
+}
+
+func (s *stack) push(d int) {
+	s.data = append(s.data, d)
+}
+
+func (s *stack) pop() int {
+	top := s.data[len(s.data)-1]
+	s.data = s.data[:len(s.data)-1]
+	return top
+}
+
+func (s *stack) top() int {
+	return s.data[len(s.data)-1]
+}
+
+func (s *stack) isEmpty() bool {
+	return len(s.data) == 0
+}
+
+func longestValidParentheses(s string) int {
+	var max int
+	st := newStack()
+	st.push(-1)
+	for i := 0; i < len(s); i++ {
+		if s[i] == ')' {
+			if st.top() == -1 || s[st.top()] == ')' {
+				st.push(i)
+			} else {
+				st.pop()
+				if length := i - st.top(); length > max {
+					max = length
+				}
+			}
+		} else {
+			st.push(i)
 		}
 	}
 	return max
