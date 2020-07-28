c9aefae264b95792754da844e05df4ffeb296587
chyeh <chyeh.taiwan@gmail.com>
2020-07-28 21:28:19 +0200

    [Update] 941 - Valid Mountain Array

--- 941_valid-mountain-array/solution.go
+++ 941_valid-mountain-array/solution.go
@@ -1,21 +1,13 @@
 package solution
 
 func validMountainArray(A []int) bool {
-	if len(A) < 3 {
-		return false
-	}
 	i := 0
-	for i < len(A)-1 && A[i] < A[i+1] {
-		i++
+	for ; i < len(A)-1 && A[i] < A[i+1]; i++ {
 	}
-	if i == len(A)-1 || i == 0 || A[i] == A[i+1] {
+	if i == 0 || i == len(A)-1 {
 		return false
 	}
-	for i < len(A)-1 && A[i] > A[i+1] {
-		i++
+	for ; i < len(A)-1 && A[i] > A[i+1]; i++ {
 	}
-	if i == len(A)-1 {
-		return true
-	}
-	return false
+	return i == len(A)-1
 }
